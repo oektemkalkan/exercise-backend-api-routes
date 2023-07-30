8bb9ab3ca3a7572d389c079c3ead869590405314
Öktem Kalkan <oektem-kalkan@t-online.de>
2023-07-30 19:46:14 +0200

    add route

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
+    <>
     <div>
       <h1>Welcome to Next.js API Routes!</h1>
     </div>
+    <Link href={"/products"}>product</Link>
+    </>
   );
 }
